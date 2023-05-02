b722ed7a0778160f9458c81527358880bcc7210c
Dmitry Redkin <tanatonaut@gmail.com>
2023-05-02 22:55:03 +0500

    test

--- scripts/prepare-contracts-package.sh
+++ scripts/prepare-contracts-package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # cd to the root of the repo
-echo "$PWD"
 cd ..
 
 #cp README.md contracts/
 # mkdir contracts/build contracts/build/contracts
 # cp -r build/contracts/*.json contracts/build/contracts
-cp -r contracts/contracts/* contracts
+mv contracts/contracts/ contracts/
+mv contracts/artifacts/contracts/ builds/
