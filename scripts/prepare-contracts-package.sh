1d5add1a73949ada11393ac0772b43baa469e6a5
Dmitry Redkin <tanatonaut@gmail.com>
2023-05-02 23:04:43 +0500

    test

--- scripts/prepare-contracts-package.sh
+++ scripts/prepare-contracts-package.sh
@@ -8,5 +8,5 @@ cd ..
 # cp -r build/contracts/*.json contracts/build/contracts
 cp -r contracts/contracts/* contracts
 mkdir builds/
-cp -r contracts/artifacts/contracts/* builds
+cp -r contracts/artifacts/contracts/** builds
 rm -rf contracts/contracts
